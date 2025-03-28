b6668398f1cdd77d0c0c80a102be2d412a220a50
Yash Kadam <kadamyash6112@gmail.com>
2025-03-28 16:07:20 +0530

    Data insert

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,5 +1,9 @@
 package com.spring.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Program started..." );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+        
+        //Insert Query
+        String query ="insert into student(id,name,city) values(?,?,?)";
+        
+        //fire query
+        //we are using template to fire query
+        int result = template.update(query,4,"demo","mumbai");
+        System.out.println("Number of record inserted "+result);
+
     }
 }
